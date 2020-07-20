4b13e235f9164080bd89b0ef0925a9de8bcd959e
David A. Parry <david.parry@igmm.ed.ac.uk>
2020-07-20 11:05:08 +0100

    Output residues as well as positions

--- res_compare.py
+++ res_compare.py
@@ -17,8 +17,12 @@ def get_align_pos(seq, p):
     return -1
 
 
-def get_p_pos(seq, i):
-    return len(seq[:i + 1].replace('-', ''))
+def align_pos_to_amino_acid(seq, i):
+    ''' Returns position within protein and the amino acid residue'''
+    if seq[i] == '-':  # no residue at this position in ortholog
+        return -1, '-'
+    p = seq[:i + 1].replace('-', '')
+    return len(p), p[-1]
 
 
 def main(ensg, pos, paralog_lookups=True):
@@ -30,9 +34,9 @@ def main(ensg, pos, paralog_lookups=True):
     data = ens_rest.get_homologies(ensg)
     if data is None:
         sys.exit("ERROR: Could not find ensembl gene '{}'".format(ensg))
-    print("\t".join('''Query_Gene Query_Protein Query_Pos Homolog_Gene
-                       Homolog_Protein Homolog_Type Species %ID
-                       Homolog_Pos'''.split()))
+    print("\t".join('''Query_Gene Query_Protein Query_Pos Query_Res
+                       Homolog_Gene Homolog_Protein Homolog_Type Species %ID
+                       Homolog_Pos Homolog_Res'''.split()))
     paralogs = parse_homology_data(data, pos)
     if paralog_lookups:
         for p in paralogs:
@@ -65,19 +69,21 @@ def parse_homology_data(data, pos, protein_id=None, skip_paralogs=False):
         s_seq = data['data'][0]['homologies'][i]['source']['align_seq']
         t_seq = data['data'][0]['homologies'][i]['target']['align_seq']
         p = get_align_pos(s_seq, pos)
-        o = get_p_pos(t_seq, p) if p > 0 else -1
+        o, aa = align_pos_to_amino_acid(t_seq, p) if p > 0 else (-1, '-')
         if 'paralog' in hom_type and o > 0:
             paralogs.append(ParalogLookup(t_id, t_protein, o))
         print("\t".join(str(x) for x in (
             s_id,
             s_protein,
             pos,
+            s_seq[p],
             t_id,
             t_protein,
             hom_type,
             data['data'][0]['homologies'][i]['target']['species'],
             data['data'][0]['homologies'][i]['target']['perc_id'],
-            o)))
+            o,
+            aa)))
     return paralogs
 
 
