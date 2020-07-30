d4f92f5741ea6156b172400f470f5d11e86dc16a
David A. Parry <david.parry@igmm.ed.ac.uk>
2020-07-30 17:53:41 +0100

    Output variant table; make seq output optional

--- utils/parse_uniprot_flat.py
+++ utils/parse_uniprot_flat.py
@@ -2,6 +2,7 @@
 import sys
 import gzip
 import re
+import argparse
 import logging
 from Bio import SwissProt
 
@@ -20,30 +21,35 @@ feature_outputs = {'CROSSLNK', 'LIPID', 'MOD_RES', 'MUTAGEN', 'SITE',
                    'VARIANT'}
 
 
-def parse_record(record, featfile, seqfile, ensfile):
+def parse_record(record, featfile, varfile, ensfile, seqfile=None):
     var_seqs = get_var_seqs(record)
     canonical = None
     display_isoform = None
-    isoforms = dict()
+    iso_vars = dict()
+    iso_seqs = dict()  # isoform to sequence inferred from var_seqs
     for match in (z for y in filter(None, (iso_re.findall(x)
                                            for x in record.comments))
                   for z in y):
         if match[2] == 'Displayed':
             canonical = match[0]
             display_isoform = match[0] + match[1]
-            isoforms[display_isoform] = record.sequence
+            if seqfile is not None:
+                iso_seqs[display_isoform] = record.sequence
         elif match[2] == 'External' or match[2] == 'Not described':
             continue
         else:
             iso = match[0] + match[1]
-            isoforms[iso] = construct_seq(iso,
-                                          record.sequence,
-                                          match[2],
-                                          var_seqs)
+            iso_vars[iso] = [x.strip() for x in match[2].split(', ')]
+            if seqfile is not None:
+                iso_seqs[iso] = construct_seq(iso,
+                                              record.sequence,
+                                              match[2],
+                                              var_seqs)
     if not canonical:
         canonical = record.accessions[0]
         display_isoform = canonical
-        isoforms[canonical] = record.sequence
+        if seqfile is not None:
+            iso_seqs[canonical] = record.sequence
     for xref in (x for x in record.cross_references if x[0] == 'Ensembl'):
         enst = xref[1]
         ensp = xref[2]
@@ -54,13 +60,15 @@ def parse_record(record, featfile, seqfile, ensfile):
         else:
             isos = [display_isoform]
         for iso in isos:
+            variants = ",".join(iso_vars.get(iso, ''))
             ensfile.write("\t".join((canonical,
                                      ensg,
                                      enst,
                                      ensp,
                                      iso,
-                                     str(int(iso == display_isoform)))) + "\n")
-    for iso, seq in isoforms.items():
+                                     str(int(iso == display_isoform)),
+                                     variants)) + "\n")
+    for iso, seq in iso_seqs.items():
         seqfile.write("\t".join((canonical,
                                  iso,
                                  str(int(iso == display_isoform)),
@@ -71,6 +79,11 @@ def parse_record(record, featfile, seqfile, ensfile):
                                   str(ft.location.start + 1),
                                   str(ft.location.end),
                                   ft.qualifiers['note'])) + "\n")
+    for var_id, var in var_seqs.items():
+        varfile.write("\t".join((var_id,
+                                 str(var['start']),
+                                 str(var['stop']),
+                                 var['seq'])) + "\n")
 
 
 def get_var_seqs(record):
@@ -111,34 +124,61 @@ def construct_seq(isoform, seq, mods, variants):
     return seq
 
 
-def main(f, prefix, progress_interval=10000, use_gzip=True):
+def main(data_file, output_prefix, progress_interval=10000, no_gzip=False,
+         output_seq=False):
     open_func = open
-    if f.endswith(".gz"):
+    if data_file.endswith(".gz"):
         open_func = gzip.open
-    write_func = gzip.open if use_gzip else open
-    suffix = 'tab.gz' if use_gzip else 'tab'
-    feat_fh = write_func(prefix + '.features.' + suffix, 'wt')
-    seq_fh = write_func(prefix + '.isoform_seqs.' + suffix, 'wt')
-    ens_fh = write_func(prefix + '.ens_ids.' + suffix, 'wt')
-    seq_fh.write("\t".join("UniprotID Isoform Displayed Seq".split()) + "\n")
+    write_func = open if no_gzip else gzip.open
+    suffix = 'tab' if no_gzip else 'tab.gz'
+    feat_fh = write_func(output_prefix + '.features.' + suffix, 'wt')
+    var_fh = write_func(output_prefix + '.seq_vars.' + suffix, 'wt')
+    ens_fh = write_func(output_prefix + '.ens_ids.' + suffix, 'wt')
+    if output_seq:
+        seq_fh = write_func(output_prefix + '.isoform_seqs.' + suffix, 'wt')
+        seq_fh.write("\t".join("UniprotID Isoform Displayed Seq".split()) + "\n")
+    else:
+        seq_fh = None
     ens_fh.write("\t".join('''UniprotID Gene Transcript Protein
-                              Isoform Displayed'''.split()) + "\n")
+                              Isoform Displayed Variants'''.split()) + "\n")
     feat_fh.write("\t".join('''UniprotID Feature Start Stop
                                Description'''.split()) + "\n")
-    with open_func(f, 'rt') as filehandle:
+    var_fh.write("\t".join('Variation Start Stop Seq'.split()) + "\n")
+    with open_func(data_file, 'rt') as filehandle:
         n = 0
         for record in SwissProt.parse(filehandle):
-            parse_record(record, feat_fh, seq_fh, ens_fh)
+            parse_record(record,
+                         featfile=feat_fh,
+                         varfile=var_fh,
+                         seqfile=seq_fh,
+                         ensfile=ens_fh)
             n += 1
             if n % progress_interval == 0:
                 logger.info("Processed {:,} records".format(n))
-    for fh in (seq_fh, feat_fh, ens_fh):
-        fh.close()
+    for fh in (seq_fh, feat_fh, ens_fh, var_fh):
+        if fh is not None:
+            fh.close()
     logger.info("Finished - processed {:,} records".format(n))
 
 
+def get_options():
+    parser = argparse.ArgumentParser(
+        description='''Parse Uniprot/Swissprot flat file into tables required
+        by ORA''')
+    parser.add_argument('data_file', help='''Uniprot flat file (gzipped is fine)
+    to process (e.g. text file from https://www.uniprot.org/downloads).''')
+    parser.add_argument('output_prefix', help='''Prefix for output files. This
+    program will create files named <prefix>.features.tab.gz,
+    <prefix>.seq_vars.tab.gz, <prefix>.ens_ids.tab.gz and optionally
+    <prefix>.isoform_seqs.tab.gz.''')
+    parser.add_argument('--output_seq', action='store_true', help='''Output an
+    additional data file with the peptide sequences of all protein isoforms.''')
+    parser.add_argument('--no_gzip', action='store_true', help='''Do not
+    compress output files with gzip.''')
+    return parser
+
+
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        sys.exit("Usage: {} <uniprot_sprot.dat.gz> <output_prefix>".format(
-            sys.argv[0]))
-    main(*sys.argv[1:])
+    argparser = get_options()
+    args = argparser.parse_args()
+    main(**vars(args))
