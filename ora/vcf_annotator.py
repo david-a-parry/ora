4e4c1b6fa98f49f2aa14998cab007c1bf1d5fb66
David A. Parry <david.parry@igmm.ed.ac.uk>
2020-10-23 13:08:14 +0100

    Use subcommands

---
+++ ora/vcf_annotator.py
@@ -0,0 +1,10 @@
+from vase.vcf_reader import VcfReader
+
+
+def vcf_annotator(args):
+
+    raise NotImplementedError("VCF annotations not implemented yet!")
+
+#    default_args = [input, db, paralog_lookups=False, line_length=60,
+#                    all_homologs=False, output_alignments=None, quiet=False,
+#                    debug=False, silent=False]
