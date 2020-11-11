4b1e24435d5081a44016b17c1f76c7ab86c6bba1
David A. Parry <david.parry@igmm.ed.ac.uk>
2020-11-11 11:49:10 +0000

    Correct HGVSp key in warning message

--- ora/vcf_annotator.py
+++ ora/vcf_annotator.py
@@ -170,7 +170,7 @@ def parse_csq(csq):
         except ValueError:
             logger.warn("Could not parse HGVSp annotation" +
                         "'{}' - falling back to  basic VEP annotations".format(
-                            csq['hgvsp']))
+                            csq['HGVSp']))
     akeys = ['ref_aa', 'var_aa', 'start', 'stop', 'description']
     result.update(dict((k, v) for k, v in zip(akeys, parse_amino_acids(csq))))
     return result
