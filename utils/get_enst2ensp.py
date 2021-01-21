cbb8dba4f3fbc3dd43cccc446b8979e6d0d47d7b
David A. Parry <david.parry@igmm.ed.ac.uk>
2021-01-21 18:01:12 +0000

    Only output transcript with ensembl protein IDs

--- utils/get_enst2ensp.py
+++ utils/get_enst2ensp.py
@@ -4,7 +4,7 @@ from biomart import BiomartServer
 
 
 def get_ens_ids(output):
-    server = BiomartServer( "http://www.ensembl.org/biomart")
+    server = BiomartServer("http://www.ensembl.org/biomart")
     ensgenes = server.datasets['hsapiens_gene_ensembl']
     response = ensgenes.search({'filters': {'biotype': 'protein_coding'},
                                 'attributes': ['ensembl_gene_id',
@@ -12,9 +12,12 @@ def get_ens_ids(output):
                                                'ensembl_peptide_id']},
                                header=1)
     if not response.ok:
-       response.raise_for_status()
+        response.raise_for_status()
     with open(output, 'wt') as fh:
-        fh.write(response.text)
+        for line in response.iter_lines(decode_unicode=True):
+            cols = line.split('\t')
+            if len(cols) == 3 and cols[2]:
+                fh.write(line + "\n")
 
 
 if __name__ == '__main__':
